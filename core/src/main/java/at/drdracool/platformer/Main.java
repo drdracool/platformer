11ac419b407d94a57173721d2d499131abd9423d
drdracool <97832811+drdracool@users.noreply.github.com>
2026-07-08 22:40:23 +0200

    Now able to send message from client to server

--- core/src/main/java/at/drdracool/platformer/Main.java
+++ core/src/main/java/at/drdracool/platformer/Main.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.net.Socket;
 import com.badlogic.gdx.Net.Protocol;
+import com.badlogic.gdx.net.SocketHints;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
@@ -14,8 +18,41 @@ public class Main extends ApplicationAdapter {
     public void create () {
         shape = new ShapeRenderer();
 
+        SocketHints hints = new SocketHints();
         Protocol protocol = Protocol.TCP;
-        Socket socket = Gdx.net.newClientSocket(protocol, "127.0.0.1", 8888, null);
+
+        Socket socket = Gdx.net.newClientSocket(protocol, "localhost", 8888, hints);
+
+        System.out.println("is connected? " + socket.isConnected());
+
+
+        DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+
+        try {
+
+
+            dOut.write("This is the first type of message.".getBytes());
+            dOut.flush(); // Send off the data
+            dOut.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        /*
+
+        try {
+
+
+            dOut.writeByte(1);
+            dOut.write("This is the first type of message.\r\n".getBytes());
+            dOut.flush(); // Send off the data
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            socket.dispose();
+        }
+         */
+
 
     }
 
